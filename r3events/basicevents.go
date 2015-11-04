c56e1a3edc169849c4294c1b88e92510c79c11e3
Bernhard Tittelbach <bernhard@tittelbach.org>
2015-11-04 12:42:23 +0100

    events

--- r3events/basicevents.go
+++ r3events/basicevents.go
@@ -2,99 +2,108 @@
 
 package r3events
 
-
 type DoorLockUpdate struct {
-    Locked bool
-    Ts int64
+	Locked bool
+	Ts     int64
 }
 
 type DoorAjarUpdate struct {
-    Shut bool
-    Ts int64
+	Shut bool
+	Ts   int64
 }
 
 type BackdoorAjarUpdate struct {
-    Shut bool
-    Ts int64
+	Shut bool
+	Ts   int64
 }
 
 type DoorCommandEvent struct {
-    Command string
-    Using string
-    Who string
-    Ts int64
+	Command string
+	Using   string
+	Who     string
+	Ts      int64
 }
 
 type DoorManualMovementEvent struct {
-    Ts int64
+	Ts int64
 }
 
 type DoorProblemEvent struct {
-    Severity int
-    Problem string
-    Ts int64
+	Severity int
+	Problem  string
+	Ts       int64
 }
 
 type BoreDoomButtonPressEvent struct {
-    Ts int64
+	Ts int64
 }
 
 type TempSensorUpdate struct {
-    Sensorindex int
-    Value float64
-    Ts int64
+	Sensorindex int
+	Value       float64
+	Ts          int64
 }
 
 type IlluminationSensorUpdate struct {
-    Sensorindex int
-    Value int64
-    Ts int64
+	Sensorindex int
+	Value       int64
+	Ts          int64
 }
 
 type DustSensorUpdate struct {
-    Sensorindex int
-    Value int64
-    Ts int64
+	Sensorindex int
+	Value       int64
+	Ts          int64
 }
 
 type RelativeHumiditySensorUpdate struct {
-    Sensorindex int
-    Percent int
-    Ts int64
+	Sensorindex int
+	Percent     int
+	Ts          int64
 }
 
 type NetDHCPACK struct {
-    Mac string
-    IP string
-    Name string
-    Ts int64
+	Mac  string
+	IP   string
+	Name string
+	Ts   int64
 }
 
 type NetGWStatUpdate struct {
-    WifiRX int32
-    WifiTX int32
-    EthernetRX int32
-    EthernetTX int32
-    InternetRX int32
-    InternetTX int32
-    NumNeigh int32
-    Ts int64
+	WifiRX     int32
+	WifiTX     int32
+	EthernetRX int32
+	EthernetTX int32
+	InternetRX int32
+	InternetTX int32
+	NumNeigh   int32
+	Ts         int64
 }
 
 type GasLeakAlert struct {
-    Ts int64
+	Ts int64
 }
 
 type TimeTick struct {
-    Ts int64
+	Ts int64
 }
 
 type MovementSensorUpdate struct {
-    Sensorindex int
-    Ts int64
+	Sensorindex int
+	Ts          int64
 }
 
 type LaserCutter struct {
-    IsHot bool
-    Ts int64
+	IsHot bool
+	Ts    int64
+}
+
+type YamahaIRCommand struct {
+	Cmd string
+	Ts  int64
+}
+
+type SendRF433Code struct {
+	Code string
+	Ts   int64
 }
--- r3events/marshal_events.go
+++ r3events/marshal_events.go
@@ -3,143 +3,153 @@
 package r3events
 
 import (
-    "encoding/json"
-    "fmt"
-    "errors"
-    "strings"
-    )
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
 
 func NameOfStruct(evi interface{}) (name string) {
-    etype := fmt.Sprintf("%T", evi)
-    etype_lastsep := strings.LastIndex(etype,".")
-    return etype[etype_lastsep+1:] //works in all cases for etype_lastsep in range -1 to len(etype)-1
+	etype := fmt.Sprintf("%T", evi)
+	etype_lastsep := strings.LastIndex(etype, ".")
+	return etype[etype_lastsep+1:] //works in all cases for etype_lastsep in range -1 to len(etype)-1
 }
 
 func MarshalEvent2ByteByte(event_interface interface{}) (data [][]byte, err error) {
-    var msg []byte
-    //~ fmt.Printf("%T%+v\n", event_interface, event_interface)
+	var msg []byte
+	//~ fmt.Printf("%T%+v\n", event_interface, event_interface)
 	msg, err = json.Marshal(event_interface)
 	if err != nil {
 		return
 	}
-    data = [][]byte{[]byte(NameOfStruct(event_interface)), msg} //works in all cases for etype_lastsep in range -1 to len(etype)-1
-    return
+	data = [][]byte{[]byte(NameOfStruct(event_interface)), msg} //works in all cases for etype_lastsep in range -1 to len(etype)-1
+	return
 }
 
 func UnmarshalByteByte2Event(data [][]byte) (event interface{}, category string, err error) {
-    if len(data) != 2 {
-        return nil, "", errors.New("not a r3event message")
-    }
-    switch string(data[0]) {
-        case "DoorLockUpdate":
-            newevent := new(DoorLockUpdate)
-            err = json.Unmarshal(data[1],newevent)
-            category = "door"
-            event = *newevent
-        case "DoorAjarUpdate":
-            newevent := new(DoorAjarUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "door"
-            event = *newevent
-        case "BackdoorAjarUpdate":
-            newevent := new(BackdoorAjarUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "door"
-            event = *newevent
-        case "DoorCommandEvent":
-            newevent := new(DoorCommandEvent)
-            err = json.Unmarshal(data[1], newevent)
-            category = "door"
-            event = *newevent
-        case "DoorProblemEvent":
-            newevent := new(DoorProblemEvent)
-            err = json.Unmarshal(data[1], newevent)
-            category = "door"
-            event = *newevent
-        case "DoorManualMovementEvent":
-            newevent := new(DoorManualMovementEvent)
-            err = json.Unmarshal(data[1], newevent)
-            category = "door"
-            event = *newevent
-        case "BoreDoomButtonPressEvent":
-            newevent := new(BoreDoomButtonPressEvent)
-            err = json.Unmarshal(data[1], newevent)
-            category = "buttons"
-            event = *newevent
-        case "TempSensorUpdate":
-            newevent := new(TempSensorUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "IlluminationSensorUpdate":
-            newevent := new(IlluminationSensorUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "DustSensorUpdate":
-            newevent := new(DustSensorUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "RelativeHumiditySensorUpdate":
-            newevent := new(RelativeHumiditySensorUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "TimeTick":
-            newevent := new(TimeTick)
-            err = json.Unmarshal(data[1], newevent)
-            category = "time"
-            event = *newevent
-        case "GasLeakAlert":
-            newevent := new(GasLeakAlert)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "MovementSensorUpdate":
-            newevent := new(MovementSensorUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "movement"
-            event = *newevent
-        case "PresenceUpdate":
-            newevent := new(PresenceUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "presence"
-            event = *newevent
-        case "SomethingReallyIsMoving":
-            newevent := new(SomethingReallyIsMoving)
-            err = json.Unmarshal(data[1], newevent)
-            category = "movement"
-            event = *newevent
-        case "TempSensorSpike":
-            newevent := new(TempSensorSpike)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "DustSensorSpike":
-            newevent := new(DustSensorSpike)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        case "NetDHCPACK":
-            newevent := new(NetDHCPACK)
-            err = json.Unmarshal(data[1], newevent)
-            category = "network"
-            event = *newevent
-        case "NetGWStatUpdate":
-            newevent := new(NetGWStatUpdate)
-            err = json.Unmarshal(data[1], newevent)
-            category = "network"
-            event = *newevent
-        case "LaserCutter":
-            newevent := new(LaserCutter)
-            err = json.Unmarshal(data[1], newevent)
-            category = "sensors"
-            event = *newevent
-        default:
-            event = nil
-            err = errors.New("cannot unmarshal unknown type")
-            category = ""
-    }
-    return
+	if len(data) != 2 {
+		return nil, "", errors.New("not a r3event message")
+	}
+	switch string(data[0]) {
+	case "DoorLockUpdate":
+		newevent := new(DoorLockUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "DoorAjarUpdate":
+		newevent := new(DoorAjarUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "BackdoorAjarUpdate":
+		newevent := new(BackdoorAjarUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "DoorCommandEvent":
+		newevent := new(DoorCommandEvent)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "DoorProblemEvent":
+		newevent := new(DoorProblemEvent)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "DoorManualMovementEvent":
+		newevent := new(DoorManualMovementEvent)
+		err = json.Unmarshal(data[1], newevent)
+		category = "door"
+		event = *newevent
+	case "BoreDoomButtonPressEvent":
+		newevent := new(BoreDoomButtonPressEvent)
+		err = json.Unmarshal(data[1], newevent)
+		category = "buttons"
+		event = *newevent
+	case "TempSensorUpdate":
+		newevent := new(TempSensorUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "IlluminationSensorUpdate":
+		newevent := new(IlluminationSensorUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "DustSensorUpdate":
+		newevent := new(DustSensorUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "RelativeHumiditySensorUpdate":
+		newevent := new(RelativeHumiditySensorUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "TimeTick":
+		newevent := new(TimeTick)
+		err = json.Unmarshal(data[1], newevent)
+		category = "time"
+		event = *newevent
+	case "GasLeakAlert":
+		newevent := new(GasLeakAlert)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "MovementSensorUpdate":
+		newevent := new(MovementSensorUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "movement"
+		event = *newevent
+	case "PresenceUpdate":
+		newevent := new(PresenceUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "presence"
+		event = *newevent
+	case "SomethingReallyIsMoving":
+		newevent := new(SomethingReallyIsMoving)
+		err = json.Unmarshal(data[1], newevent)
+		category = "movement"
+		event = *newevent
+	case "TempSensorSpike":
+		newevent := new(TempSensorSpike)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "DustSensorSpike":
+		newevent := new(DustSensorSpike)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "NetDHCPACK":
+		newevent := new(NetDHCPACK)
+		err = json.Unmarshal(data[1], newevent)
+		category = "network"
+		event = *newevent
+	case "NetGWStatUpdate":
+		newevent := new(NetGWStatUpdate)
+		err = json.Unmarshal(data[1], newevent)
+		category = "network"
+		event = *newevent
+	case "LaserCutter":
+		newevent := new(LaserCutter)
+		err = json.Unmarshal(data[1], newevent)
+		category = "sensors"
+		event = *newevent
+	case "YamahaIRCommand":
+		newevent := new(LaserCutter)
+		err = json.Unmarshal(data[1], newevent)
+		category = "actions"
+		event = *newevent
+	case "SendRF433Code":
+		newevent := new(LaserCutter)
+		err = json.Unmarshal(data[1], newevent)
+		category = "actions"
+		event = *newevent
+	default:
+		event = nil
+		err = errors.New("cannot unmarshal unknown type")
+		category = ""
+	}
+	return
 }
