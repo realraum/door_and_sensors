da2bd36a15723fde90f30a1f1566a2c0f5329592
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-03-27 15:53:04 +0200

    make_deploy

--- door_daemon/make_deploy.zsh
+++ door_daemon/make_deploy.zsh
@@ -1,7 +1,10 @@
 #!/bin/zsh
+REMOTE_HOST=torwaechter.mgmt.realraum.at
 export GO386=387
-export CGO_ENABLED=1
-go-linux-386 clean
-#go-linux-386 build
-#strip ${PWD:t}
-go-linux-386 build -ldflags "-s" && rsync -v --progress ${PWD:t} torwaechter.mgmt.realraum.at:/flash/tuer/
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=386
+
+ping -W 1 -c 1 $REMOTE_HOST || OPTIONS=(-e "ssh -o ProxyCommand='ssh gw.realraum.at exec nc %h %p'")
+go clean
+go build "$@" -ldflags "-s" && rsync ${OPTIONS[@]} -v --delay-updates --progress ${PWD:t} root@$REMOTE_HOST:/flash/tuer/
