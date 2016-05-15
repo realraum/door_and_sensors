c7cf96a6b948d7a7f078ed42356a52f74e25a957
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-05-15 13:01:23 +0200

    test

--- r3-metaevt-maker/duskordawn_test.go
+++ r3-metaevt-maker/duskordawn_test.go
@@ -19,13 +19,18 @@ func TestNextEvents(t *testing.T) {
 	if evt.name != "CityIndoorLights" && evt.havesunlight != true {
 		t.Fatalf("Unexpected next event: %+v", evt)
 	}
-	evt = calcNextSolarElevationEvent(time.Date(2016, 5, 15, 12, 00, 0, 0, time.UTC))
+	evtil := calcNextSolarElevationEvent(time.Date(2016, 5, 15, 12, 00, 0, 0, time.UTC))
 	if evt.name != "CityIndoorLights" && evt.havesunlight != false {
 		t.Fatalf("Unexpected next event: %+v", evt)
 	}
 	evt = calcNextSolarElevationEvent(time.Date(2016, 5, 15, 18, 00, 0, 0, time.UTC))
 	if evt.name != "Sunset" {
 		t.Fatalf("Unexpected next event: %+v", evt)
+	}
+	time_beween_indoorsunset_and_sunset := evt.evt_time.Sub(evtil.evt_time)
+	if time_beween_indoorsunset_and_sunset > time.Duration(83*time.Minute) ||
+		time_beween_indoorsunset_and_sunset < time.Duration(81*time.Minute) {
+		t.Fatalf("Unexptected duration between IndoorSunset and Sunset: %+v", time_beween_indoorsunset_and_sunset)
 	}
 	evt = calcNextSolarElevationEvent(time.Date(2016, 5, 15, 19, 00, 0, 0, time.UTC))
 	if evt.name != "CivilDusk" {
