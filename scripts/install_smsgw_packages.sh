fa52129525e7f4fbe47fb2e10af69c00dddfa527
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-01-04 05:47:04 +0100

    stuff

--- scripts/install_smsgw_packages.sh
+++ scripts/install_smsgw_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-apt-get install python-serial python3-pip zsh smsd libpam-systemd rsync vim tmux screen mosquitto
+apt-get install python3-serial python3-pip zsh smsd libpam-systemd rsync vim tmux screen mosquitto
 pip3 install paho-mqtt
