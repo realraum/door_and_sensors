0ac45b2c5efff60ea3896e03b597a09ed97c7fc5
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-01-22 03:57:42 +0100

    actions

--- r3events/actions.go
+++ r3events/actions.go
@@ -20,8 +20,29 @@ type SetRF433Delay struct {
 type RestartPipeLEDs struct {
 }
 
+type SturdyCeilingLight
+
 type SetPipeLEDsPattern struct {
 	Pattern string `json:"pattern"`
 	Arg     int64  `json:"arg,omitempty"`
 	Arg1    int64  `json:"arg1,omitempty"`
+}
+
+type wsDataSwitch struct {
+	Name   string
+	Action string
+}
+
+type FancyLight struct {
+	R    uint16 `json:"r,omitempty"`
+	G    uint16 `json:"g,omitempty"`
+	B    uint16 `json:"b,omitempty"`
+	CW   uint16 `json:"cw,omitempty"`
+	WW   uint16 `json:"ww,omitempty"`
+	Fade struct {
+		Duration uint16 `json:"duration,omitempty"`
+	} `json:"fade,omitempty"`
+	Flash struct {
+		Repetitions uint16 `json:"repetitions,omitempty"`
+	} `json:"flash,omitempty"`
 }
