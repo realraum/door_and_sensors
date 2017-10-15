ff57a52009481d2295c5a4c5fe028ad19cc7e8e3
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-10-15 22:57:51 +0200

    this is getting uglier and uglier with time. need to rethink all the json msgs soonish

--- r3events/marshal_events.go
+++ r3events/marshal_events.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 )
 
 func NameOfStruct(evi interface{}) (name string) {
@@ -42,99 +43,191 @@ func UnmarshalTopicByte2Event(topic string, data []byte) (event interface{}, err
 	case TOPIC_FRONTDOOR_LOCK:
 		newevent := new(DoorLockUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TOPIC_FRONTDOOR_AJAR:
 		newevent := new(DoorAjarUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_BACKDOOR_AJAR:
 		newevent := new(BackdoorAjarUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_FRONTDOOR_CMDEVT:
 		newevent := new(DoorCommandEvent)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_FRONTDOOR_PROBLEM:
 		newevent := new(DoorProblemEvent)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_FRONTDOOR_MANUALLOCK:
 		newevent := new(DoorManualMovementEvent)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case "realraum/backdoorcx/timetick":
 		newevent := new(TimeTick)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_PRESENCE:
 		newevent := new(PresenceUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_REALMOVE:
 		newevent := new(SomethingReallyIsMoving)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_TEMPSPIKE:
 		newevent := new(TempSensorSpike)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_HUMIDITYSPIKE:
 		newevent := new(HumiditySensorSpike)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_DUSTSPIKE:
 		newevent := new(DustSensorSpike)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_META_DUSKORDAWN:
 		newevent := new(DuskOrDawn)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_GW_DHCPACK:
 		newevent := new(NetDHCPACK)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_GW_STATS:
 		newevent := new(NetGWStatUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_LASER_CARD:
 		newevent := new(LaserCutter)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case ACT_YAMAHA_SEND:
 		newevent := new(YamahaIRCmd)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case ACT_RF433_SEND:
 		newevent := new(SendRF433Code)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_BACKDOOR_POWERLOSS:
 		newevent := new(UPSPowerUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_OLGAFREEZER_SENSORLOST, TOPIC_META_SENSORLOST:
 		newevent := new(SensorLost)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case ACT_RF433_SETDELAY:
 		newevent := new(SetRF433Delay)
 		err = json.Unmarshal(data, newevent)
 		event = *newevent
+
 	case TOPIC_IRCBOT_FOODREQUEST:
 		newevent := new(FoodOrderRequest)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_IRCBOT_FOODINVITE:
 		newevent := new(FoodOrderInvite)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	case TOPIC_IRCBOT_FOODETA:
 		newevent := new(FoodOrderETA)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
+
 	default:
 		event = nil
 		err = errors.New("cannot unmarshal unknown topic") // we'll never see this error, it only tells the next if-check that we want to give the next switch a try
@@ -147,49 +240,89 @@ func UnmarshalTopicByte2Event(topic string, data []byte) (event interface{}, err
 	case TYPE_DOOMBUTTON:
 		newevent := new(BoreDoomButtonPressEvent)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_TEMPOVER:
 		newevent := new(TempOverThreshold)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_TEMP:
 		newevent := new(TempSensorUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_ILLUMINATION:
 		newevent := new(IlluminationSensorUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_DUST:
 		newevent := new(DustSensorUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_RELHUMIDITY:
 		newevent := new(RelativeHumiditySensorUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_GASALERT:
 		newevent := new(GasLeakAlert)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_MOVEMENTPIR:
 		newevent := new(MovementSensorUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_POWERLOSS:
 		newevent := new(UPSPowerUpdate)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_SENSORLOST:
 		newevent := new(SensorLost)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_VOLTAGE:
 		newevent := new(Voltage)
 		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
 		event = *newevent
 	case TYPE_VENTILATIONSTATE:
 		newevent := new(VentilationState)
+		err = json.Unmarshal(data, newevent)
+		if newevent.Ts <= 0 {
+			newevent.Ts = time.Now().Unix()
+		}
+		event = *newevent
+	case TYPE_SONOFFSENSOR:
+		newevent := new(SonOffSensor)
 		err = json.Unmarshal(data, newevent)
 		event = *newevent
 	default:
