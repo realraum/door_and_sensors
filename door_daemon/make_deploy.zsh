5c25a034789f9e16dee3413d31495fa0869fc5fe
Bernhard Tittelbach <bernhard@tittelbach.org>
2020-07-08 04:18:30 +0200

    new init script name

--- door_daemon/make_deploy.zsh
+++ door_daemon/make_deploy.zsh
@@ -9,4 +9,4 @@ export GOOS=linux
 export GOARCH=386
 
 ping -W 1 -c 1 ${REMOTE_HOST} || { OPTIONS=(-o ProxyJump='gw.realraum.at:22000'); RSYNCOPTIONS=(-e "ssh $OPTIONS")}
-go build "$@" -ldflags "-s" && rsync ${RSYNCOPTIONS[@]} -rvp --delay-updates --progress --delete ${PWD:t} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/   && {echo "Restart Daemon? [Yn]"; read -q && ssh ${OPTIONS[@]} ${REMOTE_USER}@$REMOTE_HOST /etc/init.d/tuer_core restart; return 0}
+go build "$@" -ldflags "-s" && rsync ${RSYNCOPTIONS[@]} -rvp --delay-updates --progress --delete ${PWD:t} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/   && {echo "Restart Daemon? [Yn]"; read -q && ssh ${OPTIONS[@]} ${REMOTE_USER}@$REMOTE_HOST /etc/init.d/doord restart; return 0}
