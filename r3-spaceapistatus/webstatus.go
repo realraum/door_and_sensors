6e69daca7b7e20120cbd81fc1e70184ba7ef22de
Bernhard Tittelbach <bernhard@tittelbach.org>
2024-01-28 15:41:37 +0100

    advertise Mastodon Address as 'Twitter' contact since twitter is dead but spaceAPI has not caught on.

--- r3-spaceapistatus/webstatus.go
+++ r3-spaceapistatus/webstatus.go
@@ -36,7 +36,7 @@ func init() {
 	spaceapidata.AddSpaceFeed("blog", "https://wp.realraum.at/feed/", "rss")
 	spaceapidata.AddSpaceFeed("wiki", "https://realraum.at/wiki/feed.php", "rss")
 	spaceapidata.SetSpaceContactIRC("irc://irc.oftc.net/#realraum", false).SetSpaceContactMailinglist("realraum@realraum.at", false).SetSpaceContactEmail("vorstand@realraum.at", true)
-	spaceapidata.SetSpaceContactIssueMail("vorstand@realraum.at", true).SetSpaceContactTwitter("@realraum", false)
+	spaceapidata.SetSpaceContactIssueMail("vorstand@realraum.at", true).SetSpaceContactTwitter("@realraum@chaos.social", false)
 	spaceapidata.SetSpaceContactPhone("+49221596191003", false).SetSpaceContactJabber("realraum@realraum.at", false)
 	spaceapidata.AddProjectsURLs([]string{"https://chaos.social/@realraum", "https://git.github.com/realraum", "https://wiki.realraum.at/wiki/doku.php?id=projekte", "https://wp.realraum.at/", "https://git.realraum.at"})
 	if len(os.Getenv("R3_TOTAL_MEMBERCOUNT")) > 0 {
